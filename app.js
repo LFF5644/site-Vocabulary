676b9825dc1d55517b110d3033cb87b415b219dd
LFF <meerschweinchen5644@gmail.com>
2022-11-30 22:21:12 +0100

    filesystem added, hello world programm, works with windows xp!

---
+++ app.js
@@ -0,0 +1,16 @@
+function main(){
+const {
+	init,
+//	node,
+	node_dom,
+//	node_map,
+}=lui;
+
+init(()=>{
+	return[null,[
+		node_dom("h1[innerText=Vocabeln Werden Hier Später erscheinen!]"),
+		node_dom("p[innerText=Aber es functonirt alles (sugar noch unter window xp von 2001 !)]"),
+	]];
+});
+
+}//END OF MAIN!;
